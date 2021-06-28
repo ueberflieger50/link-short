d6cb3546f7ebf740fe80c4148d26c6778ff3251d
ueberflieger <ueberflieger.gaming@gmail.com>
2021-06-28 21:25:10 +0200

    ✅ Check if id already exists

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ const port = 3000
 const db = require('better-sqlite3')('links.db');
 const bodyParser = require('body-parser');
 
-
 db.exec(`CREATE TABLE IF NOT EXISTS "links" (
 	"number"	INTEGER,
 	"id"	TEXT,
 	"link"	TEXT,
 	PRIMARY KEY("number" AUTOINCREMENT)
 );`)
+
+function newId(id) {
+    if (id == undefined) {
+        id = Math.random().toString(36).slice(2, 8)
+    }
+    if (db.prepare(`SELECT id FROM links WHERE id=?`).get(id) !== undefined) {
+        return newId();
+    } else {
+        return id;
+    }
+}
 
 app.use('/', express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +31,7 @@ app.get('/:id', (req, res) => {
 });
 
 app.post('/api/new', (req, res) => {
-    let id = Math.random().toString(36).slice(2, 8)
+    let id = newId();
     db.exec(`INSERT INTO links (id, link) VALUES ('${id}', '${req.body.newUrl}')`)
     res.redirect(`/?link=${id}`);
 })
@@ -32,5 +42,5 @@ app.get('/api/all', (req, res) => {
 })
 
 app.listen(port, () => {
-	console.log(`🔗 Link Shortener listening at http://localhost:${port}`)
+    console.log(`🔗 Link Shortener listening at http://localhost:${port}`)
 })
