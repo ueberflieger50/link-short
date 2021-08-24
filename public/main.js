276ead44bcb5bfb4764a508ad8ee593d2d706ac9
ueberflieger <ueberflieger.gaming@gmail.com>
2021-08-24 23:24:17 +0200

    🎨 Link to entry with plain html link

--- public/main.js
+++ public/main.js
@@ -1,50 +1,51 @@
 const Http = new XMLHttpRequest();
 
-let url = new URL(window.location.href)
+let url = new URL(window.location.href);
 
 const app = {
-    data() {
-        return {
-            linkId: url.searchParams.get("link"),
-            currUrl: window.location.href.split("?")[0],
-            urls: null,
-        }
-    }, computed: {
-        linkUrl() {
-            return this.currUrl + this.linkId;
-        }
+  data() {
+    return {
+      linkId: url.searchParams.get("link"),
+      currUrl: window.location.href.split("?")[0],
+      urls: null,
+    };
+  },
+  computed: {
+    linkUrl() {
+      return this.currUrl + this.linkId;
     },
-    methods: {
-        getAll: function () {
-            Http.open("GET", '/api/all');
-            Http.send();
+  },
+  methods: {
+    getAll: function () {
+      Http.open("GET", "/api/all");
+      Http.send();
 
-            Http.onload = (e) => {
-                this.urls = JSON.parse(Http.responseText)
-            }
-        },
-        copyToClipboard: function(text) {
-            if (!navigator.clipboard) {
-                alert("Sorry, but your Browser dosent support Clipboard API")
-                return;
-            }
-            navigator.clipboard.writeText(text).then(() => {}, (err) => {
-                console.error('Async: Could not copy text: ', err);
-            });
-        },
-        deleteEntry: function(number, link) {
-            if(confirm('🗑 Are you sure you want to delete the link to: ' + link)){
-                Http.open("DELETE", `/api/remove/${number}`);
-                Http.send();
-    
-                Http.onload = (e) => {
-                    this.urls = JSON.parse(Http.responseText)
-                }
-            }
-        },
-        goToEntry: function(id) {
-            window.location.href = window.origin + '/' + id;
+      Http.onload = (e) => {
+        this.urls = JSON.parse(Http.responseText);
+      };
+    },
+    copyToClipboard: function (text) {
+      if (!navigator.clipboard) {
+        alert("Sorry, but your Browser dosent support Clipboard API");
+        return;
+      }
+      navigator.clipboard.writeText(text).then(
+        () => {},
+        (err) => {
+          console.error("Async: Could not copy text: ", err);
         }
-    }
-}
-Vue.createApp(app).mount('main')
+      );
+    },
+    deleteEntry: function (number, link) {
+      if (confirm("🗑 Are you sure you want to delete the link to: " + link)) {
+        Http.open("DELETE", `/api/remove/${number}`);
+        Http.send();
+
+        Http.onload = (e) => {
+          this.urls = JSON.parse(Http.responseText);
+        };
+      }
+    },
+  },
+};
+Vue.createApp(app).mount("main");
