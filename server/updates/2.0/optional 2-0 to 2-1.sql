43ad82991a7d0089dc0cba8c0d468e673aebfe40
ueberflieger <ueberflieger.gaming@gmail.com>
2021-10-07 20:46:04 +0200

    Squashed commit of the following:
    commit 760fc5fe9da05d47d9d99d5f80421c8a40d4eee0
    Author: ueberflieger <ueberflieger.gaming@gmail.com>
    Date:   Thu Oct 7 20:45:04 2021 +0200
    
        🩹 Fix update script

--- server/updates/2.0/optional 2-0 to 2-1.sql
+++ server/updates/2.0/optional 2-0 to 2-1.sql
@@ -1,6 +1,6 @@
 -- If in server dir run (terminal):
 -- sqlite3 links.db < updates/2.0/optional\ 2-0\ to\ 2-1.sql 
 
-ALTER TABLE users
+UPDATE users
 SET role = 'initAdmin'
 WHERE id = 1; -- Change the id if your initial Admin has a different id (Should not be the case)
